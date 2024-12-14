d4b57ba3611a58dbc7a1e44e725444a365cac6d7
pbusenius <14203989+pbusenius@users.noreply.github.com>
2024-12-14 21:23:17 +0100

    removed ref arg

--- evaluate.py
+++ evaluate.py
@@ -3,7 +3,6 @@ import argparse
 
 parser = argparse.ArgumentParser()
 parser.add_argument("-s", "--source", type=str, help="")
-parser.add_argument("-t", "--translation", type=str, help="")
 parser.add_argument("-r", "--reference", type=str, help="")
 args = parser.parse_args()
 
