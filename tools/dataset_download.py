593615f0983b78d12df67611f6b2b3860aa1c1c9
pbusenius <14203989+pbusenius@users.noreply.github.com>
2024-12-14 23:09:00 +0100

    ruff format

--- tools/dataset_download.py
+++ tools/dataset_download.py
@@ -2,17 +2,22 @@ import os
 import polars as pl
 
 DATA_DIR = "data"
-SPLITS = {'dev': 'dev/*.parquet', 'devtest': 'devtest/*.parquet'}
+SPLITS = {"dev": "dev/*.parquet", "devtest": "devtest/*.parquet"}
+
 
 def load_data(split: str):
-    df = pl.read_parquet('hf://datasets/openlanguagedata/flores_plus/' + SPLITS[split])
+    df = pl.read_parquet("hf://datasets/openlanguagedata/flores_plus/" + SPLITS[split])
     df = df.drop("id")
 
     for iso_code, language_df in df.group_by("iso_639_3"):
-        language_df.write_parquet(os.path.join(DATA_DIR, f"{iso_code[0]}_{split}.parquet"))
+        language_df.write_parquet(
+            os.path.join(DATA_DIR, f"{iso_code[0]}_{split}.parquet")
+        )
+
 
 def main():
     load_data("dev")
+
 
 if __name__ == "__main__":
     main()
