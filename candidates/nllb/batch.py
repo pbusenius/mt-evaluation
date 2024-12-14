99f57c2dc28e0eb813fbfe1afb329ca609f54c0a
pbusenius <14203989+pbusenius@users.noreply.github.com>
2024-12-14 23:26:19 +0100

    ruff format

--- candidates/nllb/batch.py
+++ candidates/nllb/batch.py
@@ -3,6 +3,7 @@ import polars as pl
 from typing import List
 from utils import dataset
 
+
 def nllb_batch_handler(src_languages: List[str], dst_language: str) -> pl.DataFrame:
     for src_language in src_languages:
         src_df = dataset.load_language(src_language)
