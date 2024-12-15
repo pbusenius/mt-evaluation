075d30c3bd3e49ae6d44358220d8501ea8145f86
pbusenius <14203989+pbusenius@users.noreply.github.com>
2024-12-15 22:12:43 +0100

    added load config function

--- evaluate.py
+++ evaluate.py
@@ -1,5 +1,8 @@
+import os
+import json
 import argparse
 
+from typing import Dict
 from candidates.nllb import batch
 
 parser = argparse.ArgumentParser()
@@ -8,8 +11,22 @@ parser.add_argument("-r", "--reference", type=str, help="")
 args = parser.parse_args()
 
 
+def load_settings_json(file: str) -> Dict:
+    if not os.path.exists(file):
+        return {}
+
+    with open(file) as config_file:
+        config = json.load(config_file)
+
+    return config
+
+
 def main():
-    batch.nllb_batch_handler(["ace"], "deu")
+    # batch.nllb_batch_handler(["ace"], "deu")
+
+    config = load_settings_json("settings.json")
+
+    print(config)
 
 
 if __name__ == "__main__":
