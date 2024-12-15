a05ee2cf1341c5da45648359930815aec54c6636
pbusenius <14203989+pbusenius@users.noreply.github.com>
2024-12-15 22:34:12 +0100

    removed unused code

--- candidates/nllb/batch.py
+++ candidates/nllb/batch.py
@@ -1,11 +1,5 @@
 import polars as pl
 
-from typing import List
-from utils import dataset
 
-
-def nllb_batch_handler(src_languages: List[str], dst_language: str) -> pl.DataFrame:
-    for src_language in src_languages:
-        src_df = dataset.load_language(src_language)
-
-        print(src_df)
+def batch_handler(src_language_df: pl.DataFrame, dst_language: str) -> pl.DataFrame:
+    print(src_language_df)
